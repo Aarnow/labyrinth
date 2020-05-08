7332e2ff85e38b29f8097096972596db24067c11
ArnaudFgr <arnaudfgr@gmail.com>
2020-05-09 00:57:47 +0200

    jukebox bonus 3d sound

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public bool CanOpen = false;
+    [SerializeField]
+    AudioClip soundOpen, soundDenied; //Create variable that stores different sounds
+
+    private AudioSource myAudioSource;
+
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {   
         if(other.tag == "Player" && CanOpen)
         {
             GetComponent<Animator>().enabled = true;
+            myAudioSource.PlayOneShot(soundOpen);
+        }
+        else
+        {
+            myAudioSource.PlayOneShot(soundDenied);
         }
     }
 }
