1343c9291f2a5fa1074d0c7d4777e5ea0d578cee
ArnaudFgr <arnaudfgr@gmail.com>
2020-05-09 18:56:08 +0200

    event button

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Allows access to the scenes
+
+public class UIScript : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR //Syntax differently compile
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+   
+}
